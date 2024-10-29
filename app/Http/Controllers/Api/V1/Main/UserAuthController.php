ff5039347c8b3b72f8c310906bc9fcd55b8ba9dd
Wello_Maqar <eng.waleed.atef89@gmail.com>
2024-10-30 02:00:19 +0300

    Added routes and functionalities for user register

--- app/Http/Controllers/Api/V1/Main/UserAuthController.php
+++ app/Http/Controllers/Api/V1/Main/UserAuthController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api\V1\Main;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\Min\UserRegisterRequest;
+use App\Http\Traits\ApiResponseTrait;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserAuthController extends Controller
 {
-    //
+    use ApiResponseTrait;
+
+    public function register(UserRegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken;
+        $data = [
+            'user' => $user,
+            'token' => $token
+        ];
+        return $this->apiSuccess($data);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Main\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/user/register', [UserAuthController::class, 'register']);
+});
