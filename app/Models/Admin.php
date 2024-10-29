ecdebace287cbbda53fb018c0d698e3c1a78f1ff
Wello_Maqar <eng.waleed.atef89@gmail.com>
2024-10-30 01:28:37 +0300

    Created model and migration for admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use Notifiable, HasFactory, HasApiTokens;
+
+    protected $fillable = [
+        'name',
+        'username',
+        'password',
+        'role',
+    ];
+}
