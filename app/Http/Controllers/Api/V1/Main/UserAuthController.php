fa1c410047f20077c07865412b8ac82f798d8532
Wello_Maqar <eng.waleed.atef89@gmail.com>
2024-10-30 01:39:03 +0300

    Created UserAuthRequest

--- app/Http/Controllers/Api/V1/Main/UserAuthController.php
+++ app/Http/Controllers/Api/V1/Main/UserAuthController.php
@@ -5,7 +5,4 @@ namespace App\Http\Controllers\Api\V1\Main;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UserAuthController extends Controller
-{
-    //
-}
+class UserAuthController extends Controller {}
