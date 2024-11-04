77e837d3484f36f01bc06705e5d6d8829e05c96b
Wello_Maqar <eng.waleed.atef89@gmail.com>
2024-11-04 21:44:29 +0200

    Removed editor roles form admin table and request

--- database/migrations/2024_10_29_205522_create_admins_table.php
+++ database/migrations/2024_10_29_205522_create_admins_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('password');
-            $table->enum('role', ['supervisor', 'admin', 'editor'])->default('admin');
+            $table->enum('role', ['supervisor', 'admin'])->default('admin');
             $table->timestamps();
         });
     }
