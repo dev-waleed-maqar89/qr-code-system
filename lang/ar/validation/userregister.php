33b3677f6c433e1f6c9ae968238280b18faeecee
Wello_Maqar <eng.waleed.atef89@gmail.com>
2024-11-05 18:42:58 +0200

    Added arabic language and changed app locale to arabic

---
+++ lang/ar/validation/userregister.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'name' =>
+    [
+        'required' => 'من فضلك ادخل اسمك.',
+        'string' => 'The name field must be a string.',
+        'max' => 'The name field may not be greater than 255 characters.',
+    ],
+];
